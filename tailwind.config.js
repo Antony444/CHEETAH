dc342c650b5d6901377361e57ca9d17e1e75a831
Antony444 <101749814+Antony444@users.noreply.github.com>
2024-08-22 22:07:57 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "Cairo" :["Cairo", "sans-serif"],
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
